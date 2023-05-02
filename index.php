2595c0648d4d965e07d4902568a398b8a8b2879a
DepartmentofEnvironmentandEnergy <109674863+DepartmentofEnvironmentandEnergy@users.noreply.github.com>
2023-05-02 10:37:44 -0500

    Update
    Git ignore update
    Styling update to results

--- index.php
+++ index.php
@@ -5,8 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JSON Search</title>
     <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            background-color: #4CAF50;
+            color: white;
+            padding: 1rem;
+            margin-bottom: 2rem;
+        }
+
+        input[type="text"] {
+            display: block;
+            width: 100%;
+            padding: 0.5rem;
+            font-size: 1rem;
+            box-sizing: border-box;
+        }
+
+        #results {
+            padding: 1rem;
+        }
+
         .result {
             margin-bottom: 1rem;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            padding: 1rem;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+        }
+
+        .result h3 {
+            margin-top: 0;
+            color: #4CAF50;
+        }
+
+        .result p {
+            margin: 0.5rem 0;
+        }
+
+        .result p:first-child {
+            font-weight: bold;
+        }
+
+        .result p:last-child {
+            color: #777;
         }
     </style>
 </head>
